e6586b278cb5db61cd0e9e0596c99d69ad2c8a62
aerozhou <yuhangzhou90@gmail.com>
2024-04-08 16:20:04 +0800

    Lecture 03

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Yuhang Zhou on 4/8/24.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmojiMemoryGame()
+}
